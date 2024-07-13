f5b2be282aef58913e7f298a329b631ac50216d9
tej-mahender <reddytej15@gmail.com>
2024-07-13 12:14:55 +0530

    authorization

--- Session-15/Backend-1/APIs/userApi.js
+++ Session-15/Backend-1/APIs/userApi.js
@@ -1,80 +1,93 @@
-
 const exp = require('express');
 const userApp = exp.Router();
-
+const {db}=require('mongodb')
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 //app body parser middleware
 userApp.use(exp.json())
 
-//test data
-let userData=[
-    {id:1,name:'kalki'},
-    {id:2,name:'karnan'}
-]
-
-
 //create sample rest api(req handlers-routes)
 //route for get users
-userApp.get('/users',(req,res)=>{
-    res.send({message:"all users",payload:userData})
+userApp.get('/users',async (req,res)=>{
+    //get user collection obj
+    const userCollection=req.app.get('users');
+    //get users data from collection of DB
+    let usersdata=await userCollection.find().toArray();
+    //send users data to client
+    res.send({message:"users", payload:usersdata})
 })
 
 //route to get specific user by id
-userApp.get('/users/:id',(req,res)=>{
-    //get id from url
-    let id=req.params.id; //returns id from url
-    //find user by id
-    let user=userData.find((user)=>user.id==id);
-    if(user===undefined){
-     res.send({message:'user not found'})
-    }
-    else{
-    res.send({message:'specific user',payload:user})
-    }
+userApp.get('/users/:username', async(req,res)=>{
+   //get userCollection obj
+   const userCollection=req.app.get('users');
+   //get id from url
+   const usernameURL=req.params.username;
+   //find user by id
+   let user=await userCollection.findOne({username:{$eq:{usernameURL}}});
+   //send res
+   res.send({message:"user",payload:user})
 })
 
 
 //route to post or create user
-userApp.post('/user',(req,res)=>{
-    //get new user from req
-    let newUser=req.body;
-    //add new user to array
-    userData.push(newUser);
-    //send response
-    res.send({message:'user created'})
+userApp.post('/user',async (req,res)=>{
+    const userCollection=req.app.get('users');
+    //get user data from req body
+    const user=req.body;
+
+    //verify duplicate user
+    let existUser = await userCollection.findOne({username:user.username})
+    if(existUser!==null){
+        res.send({message:"user already exist",payload:existUser})
+    }else{
+        //hash the password
+        let hashPass=await bcryptjs.hash(user.password,7)
+        //replace pass with Hashpass
+        user.password=hashPass;
+        //save user
+        await userCollection.insertOne(user)
+        //send res
+        res.send({message:"user created",payload:existUser})
+    }
+
+})
+
+userApp.post('/login',async(req,res)=>{
+
+    const userCollection=req.app.get('users');
+    //get user data from req body
+    const user=req.body;
+    //verify user
+    let dbUser=await userCollection.findOne({username:user.username})
+    if(dbUser===null){
+        res.send({message:"invalid username"})
+    } 
+    else{
+        //compare password
+        let result=await bcryptjs.compare(user.password,dbUser.password)
+        //if passwords not matched
+        if(result===false){
+            res.send({message:"invalid password"})
+            }
+            //if passwords matched
+            else{
+                //create JWT token
+            let signedToken=jwt.sign({username:user.username},'abcdef',{expiresIn:'20'})
+            //send res
+            res.send({message:"login success",token:signedToken,user:user})
+            }
+    }
 })
 
 //route to update user
-userApp.put('/user',(req,res)=>{
-    //get modified user from url
-    let modifiedUser=req.body;
-    //find index of user in userData with modified Userid
-    let index=userData.findIndex((user)=>user.id==modifiedUser.id);
-    if(index===-1){
-        res.send({message:'user not found'})
-    }
-    else{
-    //update user in array
-    userData[index]=modifiedUser;
-    //send response
-    res.send({message:'user updated'})
-    }
+userApp.put('/user',async (req,res)=>{
+
 })
 
 //route to delete user
-userApp.delete('/user/:id',(req,res)=>{
-    //get id from url
-    let id=req.params.id;
-    //find index of user in userData with id
-    let index=userData.findIndex((user)=>user.id==id);
-    if(index===-1){
-        res.send({message:'user not found'})
-        }
-        else{
-            //delete user from array
-            userData.splice(index,1);
-            //send response
-            res.send({message:'user deleted'})
-        }
+userApp.delete('/user/:id',async (req,res)=>{
+   
 })
 
 module.exports = userApp;
--- Session-15/Backend-1/server.js
+++ Session-15/Backend-1/server.js
@@ -7,8 +7,17 @@ const {MongoClient}=require('mongodb');
 let mC=new MongoClient('mongodb://localhost:27017');
 
 mC.connect()
-.then(()=>{
+.then((connectionObj)=>{
     console.log("Connected to db")
+
+    //connect to a database
+    const backend = connectionObj.db('backend_1')
+    //connect to a collection
+    const users = backend.collection('users')
+    //share collection obj to APIs
+    app.set('users',users)
+
+    //assign port number to http server of express app
     app.listen(4000,()=>console.log("http server started on port 4000"))
 })
 .catch(err=>console.log(err))
@@ -20,5 +29,3 @@ const productApp = require('./APIs/productApi');
 //if path starts with user-api forward to userApp
 app.use('/user-api',userApp)
 app.use('/product-api',productApp)
-
-//assign port number to http server of express app
