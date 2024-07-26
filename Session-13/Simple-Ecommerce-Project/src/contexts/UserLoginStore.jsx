3cd75cc6778d9d25d3ec6c22a76b91986792ac27
tej-mahender <reddytej15@gmail.com>
2024-07-26 16:19:59 +0530

    commit

--- Session-13/Simple-Ecommerce-Project/src/contexts/UserLoginStore.jsx
+++ Session-13/Simple-Ecommerce-Project/src/contexts/UserLoginStore.jsx
@@ -5,21 +5,33 @@ function UserLoginStore({children}) {
 
     let [currentUser,setCurrentUser]=useState(null)
     let [userLoginStatus,setUserLoginStatus]=useState(false)
-
+    let [err,setErr]=useState("")
     async function loginUser(userCred){
+      try{
         // let res=await fetch(`https://usersapi-pieg.onrender.com/users?username=${userCred.username}&password=${userCred.password}`)
-        let res=await fetch(`http://localhost:3000/users?username=${userCred.username}&password=${userCred.password}`)
+        let res=await fetch('http://localhost:4000/user-api/login',
+          {
+            method: 'POST',
+            headers: {"Content-type":"application/json"},
+            body:JSON.stringify(userCred)
+          }
+        )
         let data=await res.json()
-        if(data.length!==0 && data[0].password===userCred.password){
-            setCurrentUser(data[0])
+        if(data.message==="login success"){
+            setCurrentUser(data.user)
             setUserLoginStatus(true)
+            setErr('')
         }
         else{
-          console.log("Invalid Creditianls")
-          setCurrentUser(null)
+          setErr(data.message)
+          setCurrentUser({})
           setUserLoginStatus(false)
         }
     }
+    catch(error){
+      setErr(error.message)
+    }
+  }
     //user logout
     function logoutUser(){
       setCurrentUser(null)
